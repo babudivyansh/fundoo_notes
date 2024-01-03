fad84cadb23a48be614aac76cb73f31c321ef403
babudivyansh <divyansh.verma525@gmail.com>
2024-01-03 12:07:14 +0530

    [Divyansh Babu]Add: implementation of hashing the password.

--- main.py
+++ main.py
@@ -9,36 +9,9 @@
 
 @Title : Fundoo Notes using FastAPI.
 """
-from fastapi import FastAPI, status, Depends, HTTPException
-from sqlalchemy.orm import Session
-from model import User, get_db
-from schema import UserDetails, Userlogin
-from fastapi.responses import Response
-
+from fastapi import FastAPI
+from routes.user import router
 app = FastAPI()
 
+app.include_router(router, prefix='/user')
 
-@app.post("/post/", status_code=status.HTTP_201_CREATED)
-def user_registration(body: UserDetails, response: Response, db: Session = Depends(get_db)):
-    try:
-        body = body.model_dump()
-        existing_user = User(**body)
-        db.add(existing_user)
-        db.commit()
-        db.refresh(existing_user)
-        return {'message': 'user Added', 'status': 201, 'data': existing_user}
-    except Exception as e:
-        response.status_code = 400
-        return {"message": str(e), "status": 400}
-
-
-@app.post("/login/", status_code=status.HTTP_200_OK)
-def user_login(payload: Userlogin, response: Response, db: Session = Depends(get_db)):
-    try:
-        user = db.query(User).filter(User.user_name == payload.user_name and User.password == payload.password).first()
-        if not user:
-            raise HTTPException(status_code=404, detail="Invalid Credential")
-        return {'message': 'Login successful', 'status': 200, 'data': payload.model_dump()}
-    except Exception as e:
-        response.status_code = 400
-        return {'message': str(e), 'status': 400}
