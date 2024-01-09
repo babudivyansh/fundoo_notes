0f033010e31299574b573fd3d7e0359d16b88671
babudivyansh <divyansh.verma525@gmail.com>
2024-01-09 12:53:05 +0530

    [Divyansh Babu]Refactor: implementing all user APIs in this module.

--- routes/user.py
+++ routes/user.py
@@ -1,37 +1,55 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
-from model import User, get_db
-from schema import UserDetails, Userlogin
-from fastapi.responses import Response
-from pwd_hashing import Hasher
+from core.model import User, get_db
+from core.schema import UserDetails, Userlogin
+from core.utils import Hasher, JWT, send_verification_email
 
-router = APIRouter()
+router_user = APIRouter()
+jwt_handler = JWT()
 
 
-@router.post("/post/", status_code=status.HTTP_201_CREATED)
+@router_user.post("/post/", status_code=status.HTTP_201_CREATED, tags=["User"])
 def user_registration(body: UserDetails, response: Response, db: Session = Depends(get_db)):
     try:
-        body = body.model_dump()
-        body['password'] = Hasher.get_hash_password(body['password'])
-        existing_user = User(**body)
-        db.add(existing_user)
+        user_data = body.model_dump()
+        user_data['password'] = Hasher.get_hash_password(user_data['password'])
+        new_user = User(**user_data)
+        db.add(new_user)
         db.commit()
-        db.refresh(existing_user)
-        return {'message': 'user Added', 'status': 201, 'data': existing_user}
+        token = jwt_handler.jwt_encode({'user_id': new_user.id})
+        send_verification_email(token, new_user.email)
+        db.refresh(new_user)
+        return {"status": 201, "message": "Registered"}
     except Exception as e:
         response.status_code = 400
-        return {"message": str(e), "status": 400}
+        return {"message": e.args[0], 'status': 400, 'data': {}}
 
 
-@router.post("/login/", status_code=status.HTTP_200_OK)
+@router_user.post("/login/", status_code=status.HTTP_200_OK, tags=["User"])
 def user_login(payload: Userlogin, response: Response, db: Session = Depends(get_db)):
     try:
-        user = db.query(User).filter_by(user_name=payload.user_name).one_or_none()
-        if not user:
-            raise HTTPException(status_code=401, detail="Invalid Credential")
-        if not Hasher.verify_password(payload.password, user.password):
-            raise HTTPException(status_code=401, detail="Invalid Credential")
-        return {'message': 'Login successful', 'status': 200}
+        user = db.query(User).filter_by(user_name=payload.user_name).first()
+        if user.is_verified is True:
+            if user or Hasher.verify_password(payload.password, user.password):
+                token = jwt_handler.jwt_encode({'user_id': user.id})
+                return {'status': 200, "message": 'Logged in successfully', 'access_token': token}
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return {"message": 'Invalid username or password', 'status': 400, 'data': {}}
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return {"message": 'User is not verified', 'status': 400, 'data': {}}
+
     except Exception as e:
         response.status_code = 400
-        return {'message': str(e), 'status': 400}
+        return {"message": e.args[0], 'status': 400, 'data': {}}
+
+
+@router_user.get("/verify")
+def verify_user(token: str, db: Session = Depends(get_db)):
+    decode_token = JWT.jwt_decode(token)
+    user_id = decode_token.get('user_id')
+    user = db.query(User).filter_by(id=user_id).one_or_none()
+    if not user:
+        raise HTTPException(status_code=401, detail='Unauthorized User')
+    user.is_verified = True
+    db.commit()
+    return {'status': 200, "message": 'User verified successfully'}
