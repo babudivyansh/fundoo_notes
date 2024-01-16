5cd6c22464bbcdad66ae9c0aa23271c6b8a9ea2a
babudivyansh <divyansh.verma525@gmail.com>
2024-01-16 10:54:12 +0530

    [Divyansh Babu]Refactor: refactoring the env variable and adding Port, Host variable.

--- core/settings.py
+++ core/settings.py
@@ -9,3 +9,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 email_sender = getenv("EMAIL_SENDER")
 email_password = getenv("EMAIL_PASSWORD")
+
+DATABASE_PASSWORD = getenv("DATABASE_PASSWORD")
+DATABASE_NAME = getenv("DATABASE_NAME")
+
+PORT = getenv("PORT")
+HOST = getenv('localhost')
